8dad3cc98ace7481bf924a49263b977b35a7d920
Leeroyhh <aleksikaikkonenh@gmail.com>
2024-08-07 15:15:40 +0300

    changed names and added files

---
+++ osa1/t1.6/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <Button handleClick={() => setGood(good + 1)} text='good' />
+      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button handleClick={() => setBad(bad + 1)} text='bad' />
+    </div>
+  )
+}
+
+export default App
--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,13 +1,28 @@
-const App = () => {
-  const friends = [
-    { name: 'Leevi', age: 4, phone : 'lol' },
-    { name: 'Venla', age: 10, phone: '555-123456' },
-  ]
+import { useState } from 'react'
+
+
+const Display = props => <div>{props.value}</div>
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const App = (props) => {
+  const [value, setValue] = useState(10)
+
+  const setToValue = (newValue) => {
+    console.log('value now', newValue) // tulostetaan uusi arvo konsoliin
+    setValue(newValue)
+  }
 
   return (
     <div>
-      <p>{friends[0].name} {friends[0].age} {friends[0].phone}</p>
-      <p>{friends[1].name} {friends[1].age} {friends[1].phone}</p>
+      <Display value={value}/>
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
     </div>
   )
 }
