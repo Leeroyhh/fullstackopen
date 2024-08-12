7319be47ba8dd685fbe2d506d3d33026b4309d48
Leeroyhh <aleksikaikkonenh@gmail.com>
2024-08-12 16:51:41 +0300

    completed osa2/kurssitiedot

---
+++ osa2/kurssitiedot/src/components/Course.jsx
@@ -0,0 +1,42 @@
+const Title = ({ title }) => <h1>{title}</h1>;
+
+const Header = ({ courseName }) => <h2>{courseName}</h2>;
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map(part =>
+        <Part key={part.id} part={part} />
+      )}
+    </div>
+  )
+}
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+      {console.log(part)}
+    </p>
+  )
+}
+
+const Total = ({ parts }) => {
+  return parts.reduce((sum, part) => {
+    console.log('what is happening:', sum, part.exercises);
+    return sum + part.exercises;
+  }, 0)
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+        <Title title="Web development curriculum" />
+        <Header courseName={course.name} />
+        <Content parts={course.parts} />
+        <p><strong>Total exercises: {Total({ parts: course.parts })}</strong></p>
+    </div>
+  )
+}
+
+export default Course
